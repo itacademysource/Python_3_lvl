0974f6a91fa274fce31d644dbc6af4328d337efd
IT Academy/ITeen Academy <127186033+itacademysource@users.noreply.github.com>
2023-04-27 13:56:17 +0300

    Delete translater_bot/RuEnTraslater directory

--- translater_bot/RuEnTraslater/config.py
+++
@@ -1,2 +0,0 @@
-RU_SYMBOLS = r'ё1234567890-=йцукенгшщзхъ\фывапролджэячсмитьбю.Ё!"№;%:?*()_+ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ., '
-EN_SYMBOLS = r"`1234567890-=qwertyuiop[]\asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP[]ASDFGHJKL;'ZXCVBNM,./? "
